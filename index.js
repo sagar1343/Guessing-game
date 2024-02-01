460326f57fc6f9ed4d8bad904d7c95f87973ea10
Sagar <sagarchakrawarti24@gmail.com>
2024-02-01 23:13:54 +0530

    version 2.0

--- index.js
+++ index.js
@@ -9,61 +9,23 @@ const inputLetter = document.getElementById("inputLetter");
 let guessLetters = [];
 let positions = [3, 4];
 let enableForm = true;
+let index = 0;
 
-guessCountMessage.innerHTML = `You have ${guessCount} guess remaining`;
+guessCountMessage.innerHTML = showMessage(positions);
 
 showCorrectGuess(positions);
 
-form.addEventListener("submit", (event) => {
-  event.preventDefault();
-  if (!enableForm) {
-    return;
-  }
+form.addEventListener("submit", handleInput);
 
-  guessCount--;
-  if (guessCount >= 0) {
-    const updatedLetters = [...guessLetters, inputLetter.value.toUpperCase()];
-    guessLetters = [...updatedLetters];
-    displayGuessLetter(guessLetters);
-    let newPosition = matchedIndex(inputLetter.value.toUpperCase());
-    if (newPosition >= 0 && !positions.includes(newPosition)) {
-      positions = [newPosition, ...positions];
-      console.log(positions);
-      message.innerHTML = `Good guess! The word has Letter ${
-        letters[positions[0]]
-      }.`;
-    } else {
-      message.innerHTML = "Bad guess!";
-    }
-    showCorrectGuess(positions);
-    if (positions.length === letters.length) {
-      enableForm = false;
-      inputLetter.disabled = true;
-    }
-    guessCountMessage.innerHTML =
-      positions.length === letters.length
-        ? `You win!!!`
-        : `You have ${guessCount} guess remaining`;
-
-    inputLetter.value = "";
-  } else {
-    guessCountMessage.innerHTML = "You failed, try again...";
-  }
-});
-
-function matchedIndex(letter) {
-  return letters.indexOf(letter);
-}
 function showCorrectGuess(positions) {
   correctWord.innerHTML = letters
-    .map(
-      (letter, index) =>
-        `${
-          positions.includes(index)
-            ? `<span class="correct-letter" id=${index}>${letter}</span>`
-            : `<span class="correct-letter" id=${index}>*</span>`
-        }`
-    )
+    .map((letter, index) => {
+      return `${
+        positions.includes(index)
+          ? `<span class="correct-letter" id=${index}>${letter}</span>`
+          : `<span class="correct-letter" id=${index}>*</span>`
+      }`;
+    })
     .join(" ");
 }
 
@@ -71,4 +33,45 @@ function displayGuessLetter(guessLetters) {
   guessLetterDisplay.innerHTML = guessLetters
     .map((guessLetter) => `<span>${guessLetter}</span>`)
     .join(" ");
+}
+function validateLetter(letter) {
+  return letter === letters[index];
+}
+
+function handleInput(event) {
+  event.preventDefault();
+
+  if (!enableForm) {
+    return;
+  }
+  guessCount--;
+  if (guessCount >= 0) {
+    const enteredLetter = inputLetter.value.toUpperCase();
+    guessLetters = [...guessLetters, enteredLetter];
+    displayGuessLetter(guessLetters);
+
+    if (validateLetter(enteredLetter)) {
+      message.innerHTML = `Good guess! The word has Letter ${letters[index]}.`;
+      positions = [...positions, index];
+      index++;
+    } else {
+      message.innerHTML = "Bad guess!";
+    }
+    inputLetter.value = "";
+    showCorrectGuess(positions);
+    guessCountMessage.innerHTML = showMessage(positions);
+  }
+}
+function showMessage(positionArray) {
+  if (positionArray.length === letters.length) {
+    inputLetter.disabled = true;
+    enableForm = false;
+    return "You Win!!!";
+  } else if (guessCount === 0) {
+    inputLetter.disabled = true;
+    enableForm = false;
+    return "You failed, try again...";
+  } else {
+    return `You have ${guessCount} guess remaining`;
+  }
 }
